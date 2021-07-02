cbf31d7635e4b126368821051c9a41fc264d2196
Edwin Khew <edwinkhew@yahoo.com>
2021-07-01 23:33:50 -0400

    Implemented hash table using quadratic probing

---
+++ Data Structures/Hash-Table-Quadratic-Probing/HashTable.cpp
@@ -0,0 +1,182 @@
+/*
+Title: Hash Table (using quadratic probing)
+Author: Edwin Khew
+Description: Hash table class implementation.
+Date Created: 7/1/2021
+*/
+
+#include <list>
+
+template<typename ItemType>
+HashTable<ItemType>::HashTable():item_count_(0), table_size_(11)
+{
+    hash_table_ = new HashItem<ItemType> *[table_size_];
+
+    //set each index to 'nullptr'
+    for(int i = 0; i < table_size_; i++)
+    {
+        hash_table_[i] = nullptr;
+    }
+}
+
+template<typename ItemType>
+void HashTable<ItemType>::insert(int key, ItemType value)
+{
+    int address = hashFunction(key); //hash the key to get its address
+    int i = 0; //used for incrementing in quadratic probing
+
+    HashItem<ItemType> *new_item = new HashItem<ItemType>(key, value); //create a new hash item (key-value pair)
+
+    //insert at the first available (i^2)'th index found in the i'th iteration
+    while(hash_table_[(address + i * i) % table_size_] != nullptr)
+    {
+        i++;
+    }
+
+    hash_table_[(address + i * i) % table_size_] = new_item;
+
+    item_count_++;
+
+    //if the load factor exceeds the specified limit after insertion of the new item, expand and rehash the table
+    if(loadFactor() > 0.7)
+    {
+        rehashTable();
+    }
+}
+
+template<typename ItemType>
+void HashTable<ItemType>::remove(int key)
+{
+    int address = hashFunction(key); //hash the key to get its address
+    int i = 0; //used for incrementing in quadratic probing
+
+    while(hash_table_[(address + i * i) % table_size_] != nullptr) //checks all consecutive items until an empty index is reached; when an empty index is reached, then the item does not exist
+    {
+        //when an item with a matching key is found, remove it
+        if(hash_table_[(address + i * i) % table_size_]->getKey() == key)
+        {
+            hash_table_[(address + i * i) % table_size_] = nullptr;
+            break;
+        }
+
+        i++;
+    }
+}
+
+template<typename ItemType>
+void HashTable<ItemType>::clear()
+{
+    for(int i = 0; i < table_size_; i++) //loop through each index of the array
+    {
+        hash_table_[i] = nullptr;
+    }
+
+    item_count_ = 0;
+}
+
+template<typename ItemType>
+bool HashTable<ItemType>::isEmpty()
+{
+    return item_count_ == 0;
+}
+
+template<typename ItemType>
+size_t HashTable<ItemType>::itemCount()
+{
+    return item_count_;
+}
+
+template<typename ItemType>
+ItemType HashTable<ItemType>::getValue(int key)
+{
+    int address = hashFunction(key); //hash the key to get its address
+    int i = 0; //used for incrementing in quadratic probing
+
+    while(hash_table_[(address + i * i) % table_size_] != nullptr)
+    {
+        //when an item with a matching key is found, return its value
+        if(hash_table_[(address + i * i) % table_size_]->getKey() == key)
+        {
+            return hash_table_[(address + i * i) % table_size_]->getValue();
+        }
+
+        i++;
+    }
+
+    //if no matching key was found, throw an exception
+    throw(std::out_of_range("Position out of range!"));
+}
+
+template<typename ItemType>
+bool HashTable<ItemType>::contains(int key)
+{
+    int address = hashFunction(key); //hash the key to get its address
+    int i = 0; //used for incrementing in quadratic probing
+
+    while(hash_table_[(address + i * i) % table_size_] != nullptr)
+    {
+        //when an item with a matching key is found, return true
+        if(hash_table_[(address + i * i) % table_size_]->getKey() == key)
+        {
+            return true;
+        }
+
+        i++;
+    }
+
+    //if no matching key was found, return false
+    return false;
+}
+
+template<typename ItemType>
+void HashTable<ItemType>::display()
+{
+    for(int i = 0; i < table_size_; i++)
+    {
+        if(hash_table_[i] == nullptr)
+        {
+            std::cout << i << " " << std::endl;
+        }
+        else
+        {
+            std::cout << i << " " << hash_table_[i]->getValue() << std::endl;
+        }
+    }
+}
+
+template<typename ItemType>
+int HashTable<ItemType>::hashFunction(int key)
+{
+    return key % table_size_;
+}
+
+template<typename ItemType>
+double HashTable<ItemType>::loadFactor()
+{
+    return (1.0 * item_count_) / table_size_;
+}
+
+template<typename ItemType>
+void HashTable<ItemType>::rehashTable()
+{
+    HashItem<ItemType> **temp = hash_table_; //create a temporary copy of the old table
+
+    size_t temp_size = table_size_; //create a temporary copy of the old table size
+    table_size_ = 2 * table_size_; //increase the table size
+    item_count_ = 0; //reset the item count
+
+    hash_table_ = new HashItem<ItemType> *[table_size_]; //create a new array of pointers with the increased size
+
+    //loop through the temporary copy and insert each item into the new table
+    for(int i = 0; i < temp_size; i++)
+    {
+        if(temp[i] != nullptr)
+        {
+            insert(temp[i]->getKey(), temp[i]->getValue());
+        }
+    }
+
+    //delete the old table and free memory
+    delete[] temp;
+    temp = nullptr;
+}
---
+++ Data Structures/Hash-Table-Quadratic-Probing/hashTableMain.cpp
@@ -0,0 +1,67 @@
+/*
+Title: Hash Table (using quadratic probing)
+Author: Edwin Khew
+Description: Hash table test file.
+Date Created: 7/1/2021
+*/
+
+#include <iostream>
+#include "HashItem.hpp"
+#include "HashTable.hpp"
+
+using namespace std;
+
+int main()
+{
+    HashTable<string> myTable;
+
+    myTable.insert(279, "Penny");
+    myTable.insert(298, "Shelly");
+    myTable.insert(264, "Colt");
+    myTable.insert(302, "Spike");
+    myTable.insert(301, "Brock");
+    myTable.insert(287, "Edgar");
+    myTable.insert(289, "Sandy");
+    myTable.insert(304, "Leon");
+    myTable.insert(280, "Bea");
+    myTable.insert(294, "Jessie");
+    myTable.insert(295, "Tara");
+    myTable.insert(334, "Rosa");
+    myTable.insert(292, "Crow");
+    myTable.insert(300, "Lou");
+
+    //myTable.remove(32);
+
+    //Clustering test
+/*
+    myTable.insert(3, "A");
+    myTable.insert(14, "B");
+    myTable.insert(25, "C");
+    myTable.insert(36, "D");
+    myTable.insert(47, "E");
+    myTable.insert(58, "G");
+*/
+
+    cout << "Is Empty: " << myTable.isEmpty() << endl;
+    cout << "Item Count: " << myTable.itemCount() << endl;
+    cout << "Contains: " << myTable.contains(280) << endl;
+
+    try
+    {
+        cout << "Get Value: " << myTable.getValue(304) << endl;
+    }
+    catch(const std::out_of_range& problem)
+    {
+        cout << "Item not found!" << endl;
+    }
+
+    cout << "Display: " << endl << endl;
+    myTable.display();
+
+    myTable.clear();
+
+    cout << endl << "Is Empty (After clear): " << myTable.isEmpty() << endl;
+    cout << "Item Count (After clear): " << myTable.itemCount() << endl;
+    cout << "Display (After clear): " << endl << endl;
+    myTable.display();
+}
